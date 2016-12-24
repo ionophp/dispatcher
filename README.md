ffe4b21b84b8a6e9c41f1f5cd939a2c7adeeff5c
ytake <yuuki.takezawa@comnect.jp.net>
2016-12-25 02:42:15 +0900

    for ci

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# Iono.Dispatcher
+# Iono\Dispatcher
 
+[![Build Status](https://travis-ci.org/ionophp/dispatcher.svg?branch=master)](https://travis-ci.org/ionophp/dispatcher)
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/c7d9d3bdfe22478c8872ed92d3d65359)](https://www.codacy.com/app/ionophp/dispatcher?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=ionophp/dispatcher&amp;utm_campaign=Badge_Grade)
 [![Coverage Status](https://coveralls.io/repos/github/ionophp/dispatcher/badge.svg?branch=master)](https://coveralls.io/github/ionophp/dispatcher?branch=master)
+
+[![StyleCI](https://styleci.io/repos/77292193/shield?branch=master)](https://styleci.io/repos/77292193)
 
 
 PHP flux pattern dispatcher library(facebook style)  
--- src/invariant.php
+++ src/invariant.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 /**
  * Invariant.php
  * facebook flux pattern style
- * @author yuuki.takezawa<yuuki.takezawa@comnect.jp.net>
+ *
+ * @author  yuuki.takezawa<yuuki.takezawa@comnect.jp.net>
  * @license http://opensource.org/licenses/MIT MIT
  */
 namespace Iono\Dispatcher;
@@ -12,18 +13,26 @@ namespace Iono\Dispatcher;
 use Iono\Dispatcher\Exception\InvariantException;
 
 /**
- * @param null $condition
+ * @param null   $condition
  * @param string $format
- * @param $a
- * @param $b
- * @param $c
- * @param $d
- * @param $e
- * @param $f
+ * @param        $a
+ * @param        $b
+ * @param        $c
+ * @param        $d
+ * @param        $e
+ * @param        $f
+ *
  * @throws InvariantException
  */
 function invariant(
-    $condition = null, $format = '', $a = null, $b = null, $c = null, $d = null, $e = null, $f = null
+    $condition = null,
+    $format = '',
+    $a = null,
+    $b = null,
+    $c = null,
+    $d = null,
+    $e = null,
+    $f = null
 ) {
     if (false) {
         if ($format == '') {
@@ -53,7 +62,7 @@ function invariant(
             );
         }
     }
-    if($error instanceof InvariantException) {
+    if ($error instanceof InvariantException) {
         $error->framesToPop = 1;
         throw $error;
     }
